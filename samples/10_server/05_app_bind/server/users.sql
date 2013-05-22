898fb41268696e487a7140d535041a78a2ce6937
ghubtny <student@lab308-03.local>
2013-05-22 15:37:15 +0700

    Hello

--- samples/10_server/05_app_bind/server/users.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS "tblusers";
-CREATE TABLE "tblusers" ("id" INTEGER PRIMARY KEY  NOT NULL , "name" VARCHAR(60), age VARCHAR(3), "country" VARCHAR(60), "city" VARCHAR(60), "gender" VARCHAR(6), "phone" VARCHAR(60), "email" VARCHAR(60));
